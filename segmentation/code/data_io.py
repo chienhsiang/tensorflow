cdf357708a135bd4575afc4140ba5e84e3dc9fc5
Chien-Hsiang Hsu <hsuchienhsiang@gmai.com>
2019-04-05 08:24:28 -0700

    Organized code.

---
+++ segmentation/code/data_io.py
@@ -0,0 +1,102 @@
+"""Module for data IO, preprocessing, and augmentation"""
+
+__author__ = 'Chien-Hsiang Hsu'
+__create_date__ = '2019.04.05'
+
+
+
+import functools
+import tensorflow as tf
+
+
+MAX_INTENSITY = 4095.
+
+
+# Get image and mask from path name
+def _get_image_from_path(img_path, mask_path):
+    img = tf.image.decode_png(tf.io.read_file(img_path), channels=1, dtype=tf.uint16)
+    img = tf.image.convert_image_dtype(tf.cast(img, tf.float32) / MAX_INTENSITY, tf.uint8)
+    mask = tf.image.decode_jpeg(tf.io.read_file(mask_path), channels=1)
+    
+    # Remove bounday 100 pixels since masks touching boundaries were removed
+    w = 100
+    img = img[w:-w,w:-w,:]
+    mask = mask[w:-w,w:-w,:]
+    
+    return img, mask
+
+
+"""
+Data augmentation
+"""
+# Flip images
+def flip_images(to_flip, img, mask):
+    """Flip image and mask horizonally and vertically with prob = 0.5 (separately)"""
+    if to_flip:
+        flip_prob = tf.random.uniform([2], 0, 1) # [horizontal, vertical]
+        
+        # flip horizontally
+        img, mask = tf.cond(tf.less(flip_prob[0], 0.5), 
+                            lambda: (tf.image.flip_left_right(img), 
+                                     tf.image.flip_left_right(mask)),
+                            lambda: (img, mask))
+        # flip vertically
+        img, mask = tf.cond(tf.less(flip_prob[1], 0.5), 
+                            lambda: (tf.image.flip_up_down(img), 
+                                     tf.image.flip_up_down(mask)),
+                            lambda: (img, mask))
+    return img, mask
+
+
+# Random crop
+def random_crop(img, mask, size=[500, 700]):
+    if size is not None:
+        assert len(size) == 2, "size must have 2 elments"
+        # Combine image and maks then crop
+        comb = tf.concat([img, mask], axis=2)
+        crop_size = comb.shape.as_list()
+        crop_size[:2] = size
+        comb = tf.image.random_crop(comb, size=crop_size)
+
+        # Take out copped image and mask
+        img_dim = img.shape[-1]
+        img = comb[:,:,:img_dim]
+        mask = comb[:,:,img_dim:]
+    
+    return img, mask
+
+
+# Assembled augmentation function
+def _augment(img, mask, resize=None, scale=1., crop_size=None, to_flip=False):
+    if resize is not None:
+        img = tf.image.resize(img, size=resize)
+        mask = tf.image.resize(mask, size=resize)
+    
+    # Crop and flip
+    img, mask = random_crop(img, mask, size=crop_size)
+    img, mask = flip_images(to_flip, img, mask)
+    
+    # Scale the intensity
+    img = tf.cast(img, tf.float32) * scale
+    mask = tf.cast(mask, tf.float32) * scale
+    
+    return img, mask
+
+
+
+"""
+Input pipeline
+"""
+def get_dataset(img_paths, mask_paths, preproc_fn=functools.partial(_augment),
+                shuffle=False, batch_size=1, threads=5):
+    dataset = tf.data.Dataset.from_tensor_slices((img_paths, mask_paths))
+    dataset = dataset.map(_get_image_from_path, num_parallel_calls=threads)    
+    dataset = dataset.map(preproc_fn, num_parallel_calls=threads)
+    
+    if shuffle:
+        n_samples = len(img_paths)
+        dataset = dataset.shuffle(n_samples)
+    
+    dataset = dataset.repeat().batch(batch_size)
+    
+    return dataset  
---
+++ segmentation/code/u_net.py
@@ -0,0 +1,135 @@
+"""Define Unet
+
+Based on the tutorial of tensorflow.
+"""
+
+__author__ = 'Chien-Hsiang Hsu'
+__create_date__ = '2019.04.05'
+
+
+import tensorflow as tf
+from tensorflow.keras import models, layers, losses
+
+
+"""
+Define layers and model
+"""
+class Conv_block(layers.Layer):
+    def __init__(self, num_filters, name='conv_block'):
+        super().__init__(name=name)
+        self.num_filters = num_filters
+        self.conv2d_1 = layers.Conv2D(num_filters, (3, 3), padding='same')
+        self.bn_1 = layers.BatchNormalization()
+        self.activation_1 = layers.Activation('relu')
+        self.conv2d_2 = layers.Conv2D(num_filters, (3, 3), padding='same')
+        self.bn_2 = layers.BatchNormalization()
+        self.activation_2 = layers.Activation('relu')
+        
+    def call(self, inputs):
+        x = self.conv2d_1(inputs)
+        x = self.bn_1(x)
+        x = self.activation_1(x)
+        x = self.conv2d_2(x)
+        x = self.bn_2(x)
+        x = self.activation_2(x)
+        return x
+
+
+class Encoder(layers.Layer):
+    def __init__(self, num_filters, name='encoder'):
+        super().__init__(name=name)
+        self.num_filters = num_filters
+        self.conv_block = Conv_block(num_filters)
+        self.mp2d = layers.MaxPool2D((2, 2), strides=(2, 2))
+        
+    def call(self, inputs):
+        x = self.conv_block(inputs)
+        x_pool = self.mp2d(x)
+        return x_pool, x
+
+
+class Decoder(layers.Layer):
+    def __init__(self, num_filters, name='decoder'):
+        super().__init__(name=name)
+        self.num_filters = num_filters
+        self.conv2d_tr = layers.Conv2DTranspose(num_filters, (2, 2), 
+                                                strides=(2, 2), padding='same')
+        self.bn = layers.BatchNormalization()
+        self.activation = layers.Activation('relu')
+        self.conv_block = Conv_block(num_filters)
+        
+    def call(self, inputs, concat_tensor):
+        x = self.conv2d_tr(inputs)
+        x = layers.concatenate([x, concat_tensor], axis=-1)
+        x = self.bn(x)
+        x = self.activation(x)
+        x = self.conv_block(x)
+        return x
+
+
+class Unet(models.Model):
+    def __init__(self, n_filters_list, name='u_net'):
+        assert type(n_filters_list) is list, "n_filters_list must be a list"
+        super().__init__(name=name)
+        self.conv_block_center = Conv_block(n_filters_list[-1] * 2)
+        self.conv_block_final = Conv_block(n_filters_list[0])
+        self.con2d_1x1 = layers.Conv2D(1, (1, 1), activation='sigmoid')        
+        
+        self.encoders = [Encoder(n_filters) for n_filters in n_filters_list]
+        self.decoders = [Decoder(n_filters) for n_filters in reversed(n_filters_list)]
+        
+    def call(self, inputs):
+        # Encoding cascade
+        x_pools = []
+        xs = []
+        for i, encoder in enumerate(self.encoders):
+            if i == 0:
+                tmp_pool, tmp = encoder(inputs)                
+            else:
+                tmp_pool, tmp = encoder(x_pools[i-1])
+                
+            x_pools.append(tmp_pool)
+            xs.append(tmp)
+        
+        # Center convolution block
+        z = self.conv_block_center(x_pools[-1])
+        
+        # Decoding cascade
+        xs = xs[::-1] # to match the order of decoders
+        for i, decoder in enumerate(self.decoders):
+            z = decoder(z, xs[i])
+        
+        # Final 1x1 convolution
+        z = self.conv_block_final(z)
+        z = self.con2d_1x1(z)
+        return z
+
+
+"""
+Loss function
+"""
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true = tf.reshape(y_true, shape=[-1])
+    y_pred = tf.reshape(y_pred, shape=[-1])
+    
+    intersect = tf.reduce_sum(y_true * y_pred)
+    score = (2. * intersect + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
+    return score
+
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coeff(y_true, y_pred)
+
+def bce_dice_loss(y_true, y_pred):
+    loss = losses.binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+
+# Get model
+def get_model(num_filters_list=[32, 64, 128, 256, 512], compiled=True,
+			  optimizer='adam', loss='bce_dice_loss',
+			  metrics=[dice_loss]):
+	model = Unet(num_filters_list)
+	if compiled:
+		model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+	return model
