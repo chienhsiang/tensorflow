9e8eee56d8766c269a2320223a4d5812eca9a915
Chien-Hsiang Hsu <hsuchienhsiang@gmai.com>
2019-05-02 15:15:38 -0700

    Done organizing

--- segmentation/code/segmentation.py
+++ segmentation/code/segmentation.py
@@ -48,10 +48,17 @@ class Task:
         # merge these two configs
         cfg = {**common_cfg, **cfg}
 
-        cfg['img_dir'] = os.path.join(cfg['root_folder'], cfg['data_subfoler'],
-                                      cfg['train_data'],  cfg['img_subfolder'])
-        cfg['mask_dir'] = os.path.join(cfg['root_folder'], cfg['data_subfoler'],
-                                      cfg['train_data'],  cfg['mask_subfolder'])
+        if 'train_data' in cfg:
+            cfg['img_dir'] = os.path.join(cfg['root_folder'], cfg['data_subfoler'],
+                                          cfg['train_data'], cfg['img_subfolder'])
+            cfg['mask_dir'] = os.path.join(cfg['root_folder'], cfg['data_subfoler'],
+                                           cfg['train_data'], cfg['mask_subfolder'])
+
+        if 'test_data' in cfg:
+            cfg['test_img_dir'] = os.path.join(cfg['root_folder'], cfg['data_subfoler'],
+                                               cfg['test_data']['name'], cfg['img_subfolder'])
+            cfg['test_mask_dir'] = os.path.join(cfg['root_folder'], cfg['data_subfoler'],
+                                               cfg['test_data']['name'], cfg['mask_subfolder'])
         
         # conversion string to number or function handle
         if 'read_cfg' in cfg:
@@ -59,6 +66,9 @@ class Task:
         
         if 'metrics' in cfg:
             cfg['metrics'] = [eval(s) for s in cfg['metrics']]
+
+        if 'test_read_cfg' in cfg:
+            cfg['test_read_cfg']['scale'] = eval(cfg['test_read_cfg']['scale'])
             
         return cfg  
 
@@ -141,7 +151,7 @@ class Task:
         return model
 
 
-    def training_model(self):
+    def train_model(self):
         cfg = self.cfg
         monitor = cfg['monitor']
         epochs = cfg['epochs']
@@ -175,6 +185,110 @@ class Task:
                             validation_steps=int(np.ceil(num_val_data / batch_size)),
                             callbacks=[cp, tb])
 
+
+    def eval_model(self):
+        train_ds, val_ds, num_train_data, num_val_data, batch_size = self.get_train_val_dataset()
+        model = self.get_trained_model()
+
+        # Export predicted images
+        result_folder = os.path.join(self.get_result_dir(), "EVAL")
+        if not os.path.isdir(result_folder):
+            os.makedirs(result_folder)
+
+        idx_to_plot = np.random.choice(num_val_data, n_test)
+        for i, (img, mask) in enumerate(val_ds):
+            if i in idx_to_plot:
+                y_pred = model(img)        
+                for j in range(batch_size-1):
+                    I = np.uint8(img[j].numpy()*255.)
+                    M = np.uint8(mask[j].numpy()*255.) 
+                    M_pred = np.uint8((y_pred[j].numpy() > 0.5) *255.)
+                    
+                    if task == 'both_seg':
+                        I = np.uint8(img[j].numpy()*255.)
+                        M = np.uint8(mask[j].numpy()*255.) 
+                        M_pred = np.uint8((y_pred[j].numpy() > 0.5) *255.)
+                        
+                        # overlay nucleus segmentation
+                        I = data_io.overlay_mask(I, M[:,:,nuc_idx], M_pred[:,:,nuc_idx], 
+                                         true_color=None, pred_color=(0,255,255))
+                        # overlay cell segmentation
+                        I = data_io.overlay_mask(I, M[:,:,cell_idx], M_pred[:,:,cell_idx], 
+                                         true_color=None, pred_color=(255,0,255))
+                    else:
+                        I = data_io.overlay_mask(I, M[:,:,0], M_pred[:,:,0])
+                    
+                    fname = os.path.join(result_folder, '{}_{}.png'.format(i,j))
+                    cv2.imwrite(fname, cv2.cvtColor(I, cv2.COLOR_RGB2BGR))
+                    
+            if i > max(idx_to_plot):
+                print("Save results at:")
+                print('  ', result_folder)
+                break
+
+
+    def test_model(self, test_yaml):
+        test_cfg = self.get_cfg_from_yaml(test_yaml)
+
+        file_dir = test_cfg['test_img_dir']
+        file_type = test_cfg['test_data']['file_type']
+        filter_patter = test_cfg['test_data']['filter_patter']
+        chunk_size = test_cfg['chunk_size']
+        test_read_cfg = test_cfg['test_read_cfg']
+
+        # Get test dataset and break into chunks (memory issue)
+        img_files = data_io.get_filenames(file_dir, file_type, filter_patter)
+        img_files = [img_files[i:i+chunk_size] for i in range(0, len(img_files), chunk_size)]
+        if test_cfg['show_ans']:
+            ans_dir = test_cfg['test_mask_dir']
+            ans_files = data_io.get_filenames(ans_dir, file_type, filter_patter)
+            ans_files = [ans_files[i:i+chunk_size] for i in range(0, len(ans_files), chunk_size)]
+
+        read_img_fn = functools.partial(data_io._get_image_from_path, **test_read_cfg)
+
+        # Get trained model
+        model = self.get_trained_model()
+
+        # Output the images
+        result_folder = os.path.join(self.get_result_dir(), test_cfg['test_data']['name'])
+        if not os.path.isdir(result_folder):
+            os.makedirs(result_folder)
+
+        for i, g in enumerate(img_files):
+            print()
+            print("Predicting chunck {}/{}...".format(i+1, len(img_files)))
+            test_ds = data_io.get_dataset(g, None, read_img_fn=read_img_fn,
+                                          shuffle=False, repeat=False, batch_size=1)
+            y_pred = model.predict(test_ds, verbose=1)
+
+            if test_cfg['show_ans']:
+                ans_ds = data_io.get_dataset(ans_files[i], None, read_img_fn=read_img_fn,
+                                             shuffle=False, repeat=False, batch_size=1)
+            else:
+                holder = [[] for i in range(len(g))]
+                ans_ds = tf.data.Dataset.from_tensor_slices(holder)
+
+            test_ds = tf.data.Dataset.zip((test_ds, ans_ds))
+
+            for j, (x, y) in enumerate(test_ds):
+                print("Saving results {}/{}...".format(j+1, len(g)), end='\r')
+                I = np.uint8(x[0]*255.)
+                M_pred = np.uint8((y_pred[j,...,0] > 0.5) * 255.)
+
+                if test_cfg['show_ans']:
+                    M = []
+                    true_color = None
+                else:
+                    M = np.uint8((y[0].numpy() > 0.5) * 255.)
+                    true_color = (0,255,0)
+
+                I = data_io.overlay_mask(I, M, M_pred, true_color=true_color, pred_color=(255,0,0))
+                
+                fname = os.path.join(result_folder, os.path.basename(g[j]))
+                cv2.imwrite(fname, cv2.cvtColor(I, cv2.COLOR_RGB2BGR))
+            print()
+
+
     #-----------------------------------------------------------------------------------------------
     # Helper functions
     #-----------------------------------------------------------------------------------------------
@@ -186,6 +300,10 @@ class Task:
         cfg = self.cfg
         return os.path.join(cfg['root_folder'], cfg['log_subfolder'], cfg['model_name'])
 
+    def get_result_dir(self):
+        cfg = self.cfg
+        return os.path.join(cfg['root_folder'], cfg['result_subfolder'], cfg['model_name'])
+
 
 
 ####################################################################################################
@@ -196,6 +314,7 @@ if __name__ == '__main__':
     parser.add_argument("task_yaml", help="yaml file of the task")
     parser.add_argument("-mode", dest="mode", help="TRAIN, EVAL or TEST", default="TRAIN")
     parser.add_argument("-gpu_id", dest="gpu", help="ID of GPU to use", default='0')
+    parser.add_argument("-test_yaml", dest="test_yaml", help="Yaml of test data", default='')
 
     args = parser.parse_args()
 
@@ -206,100 +325,13 @@ if __name__ == '__main__':
     task = Task(args.task_yaml)
 
     if MODE == 'TRAIN':
-        task.training_model()
+        task.train_model()
 
-    # if MODE == 'EVAL':
-    #     task.training_model()
+    if MODE == 'EVAL':
+        task.eval_model()
 
-
-
-    # if MODE == 'EVAL':
-    #     train_ds, val_ds, num_train_data, num_val_data, batch_size = get_train_val_dataset()
-    #     model = get_trained_model()
-
-    #     # Export predicted images
-    #     if not os.path.isdir(result_folder):
-    #         os.makedirs(result_folder)
-
-    #     idx_to_plot = np.random.choice(num_val_data, n_test)
-    #     for i, (img, mask) in enumerate(val_ds):
-    #         if i in idx_to_plot:
-    #             y_pred = model(img)        
-    #             for j in range(batch_size-1):
-    #                 I = np.uint8(img[j].numpy()*255.)
-    #                 M = np.uint8(mask[j].numpy()*255.) 
-    #                 M_pred = np.uint8((y_pred[j].numpy() > 0.5) *255.)
-                    
-    #                 if task == 'both_seg':
-    #                     I = np.uint8(img[j].numpy()*255.)
-    #                     M = np.uint8(mask[j].numpy()*255.) 
-    #                     M_pred = np.uint8((y_pred[j].numpy() > 0.5) *255.)
-                        
-    #                     # overlay nucleus segmentation
-    #                     I = data_io.overlay_mask(I, M[:,:,nuc_idx], M_pred[:,:,nuc_idx], 
-    #                                      true_color=None, pred_color=(0,255,255))
-    #                     # overlay cell segmentation
-    #                     I = data_io.overlay_mask(I, M[:,:,cell_idx], M_pred[:,:,cell_idx], 
-    #                                      true_color=None, pred_color=(255,0,255))
-    #                 else:
-    #                     I = data_io.overlay_mask(I, M[:,:,0], M_pred[:,:,0])
-                    
-    #                 fname = os.path.join(result_folder, '{}_{}.png'.format(i,j))
-    #                 cv2.imwrite(fname, cv2.cvtColor(I, cv2.COLOR_RGB2BGR))
-                    
-    #         if i > max(idx_to_plot):
-    #             print("Save results at:")
-    #             print('  ', result_folder)
-    #             break
-
-    # if MODE == 'TEST':
-    #     # Get test dataset and break into chunks (memory issue)
-    #     img_files = data_io.get_filenames(file_dir, file_type, filter_patter)
-    #     img_files = [img_files[i:i+chunk_size] for i in range(0, len(img_files), chunk_size)]
-    #     if ans_dir is not None:
-    #         ans_files = data_io.get_filenames(ans_dir, file_type, filter_patter)
-    #         ans_files = [ans_files[i:i+chunk_size] for i in range(0, len(ans_files), chunk_size)]
-
-    #     read_img_fn = functools.partial(data_io._get_image_from_path, **test_read_cfg)
-
-    #     # Get trained model
-    #     model = get_trained_model()
-
-    #     # Output the images
-    #     if not os.path.isdir(result_folder):
-    #         os.makedirs(result_folder)
-
-    #     for i, g in enumerate(img_files):
-    #         print()
-    #         print("Predicting chunck {}/{}...".format(i+1, len(img_files)))
-    #         test_ds = data_io.get_dataset(g, None, read_img_fn=read_img_fn,
-    #                                       shuffle=False, repeat=False, batch_size=1)
-    #         y_pred = model.predict(test_ds, verbose=1)
-
-    #         if ans_dir is not None:
-    #             ans_ds = data_io.get_dataset(ans_files[i], None, read_img_fn=read_img_fn,
-    #                                          shuffle=False, repeat=False, batch_size=1)
-    #         else:
-    #             holder = [[] for i in range(len(g))]
-    #             ans_ds = tf.data.Dataset.from_tensor_slices(holder)
-
-    #         test_ds = tf.data.Dataset.zip((test_ds, ans_ds))
-
-    #         for j, (x, y) in enumerate(test_ds):
-    #             print("Saving results {}/{}...".format(j+1, len(g)), end='\r')
-    #             I = np.uint8(x[0]*255.)
-    #             M_pred = np.uint8((y_pred[j,...,0] > 0.5) * 255.)
-
-    #             if ans_dir is None:
-    #                 M = []
-    #                 true_color = None
-    #             else:
-    #                 M = np.uint8((y[0].numpy() > 0.5) * 255.)
-    #                 true_color = (0,255,0)
-
-    #             I = data_io.overlay_mask(I, M, M_pred, true_color=true_color, pred_color=(255,0,0))
-                
-    #             fname = os.path.join(result_folder, os.path.basename(g[j]))
-    #             cv2.imwrite(fname, cv2.cvtColor(I, cv2.COLOR_RGB2BGR))
-    #         print()
+    if MODE == 'TEST':
+        assert args.test_yaml is not '', "Need to specify test yaml"
+        task.test_model(args.test_yaml)
+        
     
